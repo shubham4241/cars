d651f451bd4711d864414c7f399ba3618e67bf8f
Shubham Singh <you@example.com>
2022-08-24 02:20:21 +0530

    car

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarComponent } from './car/car.component';
+import { ConvertibleComponent } from './convertible/convertible.component';
+import { HatchbackComponent } from './hatchback/hatchback.component';
 import { SedanComponent } from './sedan/sedan.component';
 
 const routes: Routes = [
-  {path:'sedan', component:SedanComponent
+  {path:'sedan', 
+  component:SedanComponent,
+  children:[{
+    path:'car', 
+    component:CarComponent,
+  }]
+},
+{path:'hatchback', 
+  component:HatchbackComponent,
+  children:[{
+    path:'car', 
+    component:CarComponent,
+  }]
+},
+{path:'convertible', 
+  component:ConvertibleComponent,
+  children:[{
+    path:'car', 
+    component:CarComponent,
+  }]
 },
 ];
 
--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -12,11 +12,25 @@ export class CarComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  icars =[
+  cs =[
     {id:1,model: 'Honda Accord', year:'2022',price:38050, img:'https://cars.usnews.com/pics/size/390x290/images/Auto/izmo/i159614825/2022_honda_accord_sedan_angularfront.jpg'},
     {id:2,model: 'Hyundai Sonata', year:'2022',price:34750, img:'https://cars.usnews.com/pics/size/390x290/images/Auto/custom/14725/2022_Hyundai_Sonata_1.jpg'},
     {id:3,model: 'Kia K5', year:'2022',price:31190, img:'https://cars.usnews.com/pics/size/390x290/images/Auto/izmo/i159614616/2022_kia_k5_angularfront.jpg'},
     {id:4,model: 'Nissan Altima', year:'2022',price:34450, img:'https://cars.usnews.com/pics/size/390x290/images/Auto/izmo/i159614887/2022_nissan_altima_angularfront.jpg'},
      ];
+
+     ch =[
+      {id:1,model: 'Honda Accord', year:'2022',price:38050, img:'https://cars.usnews.com/pics/size/390x290/images/Auto/izmo/i159614825/2022_honda_accord_sedan_angularfront.jpg'},
+      {id:2,model: 'Hyundai Sonata', year:'2022',price:34750, img:'https://cars.usnews.com/pics/size/390x290/images/Auto/custom/14725/2022_Hyundai_Sonata_1.jpg'},
+      {id:3,model: 'Kia K5', year:'2022',price:31190, img:'https://cars.usnews.com/pics/size/390x290/images/Auto/izmo/i159614616/2022_kia_k5_angularfront.jpg'},
+      {id:4,model: 'Nissan Altima', year:'2022',price:34450, img:'https://cars.usnews.com/pics/size/390x290/images/Auto/izmo/i159614887/2022_nissan_altima_angularfront.jpg'},
+       ];
+
+       cc =[
+        {id:1,model: 'Honda Accord', year:'2022',price:38050, img:'https://cars.usnews.com/pics/size/390x290/images/Auto/izmo/i159614825/2022_honda_accord_sedan_angularfront.jpg'},
+        {id:2,model: 'Hyundai Sonata', year:'2022',price:34750, img:'https://cars.usnews.com/pics/size/390x290/images/Auto/custom/14725/2022_Hyundai_Sonata_1.jpg'},
+        {id:3,model: 'Kia K5', year:'2022',price:31190, img:'https://cars.usnews.com/pics/size/390x290/images/Auto/izmo/i159614616/2022_kia_k5_angularfront.jpg'},
+        {id:4,model: 'Nissan Altima', year:'2022',price:34450, img:'https://cars.usnews.com/pics/size/390x290/images/Auto/izmo/i159614887/2022_nissan_altima_angularfront.jpg'},
+         ];
 
 }
